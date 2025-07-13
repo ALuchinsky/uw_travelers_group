5080ddf278bd4edf105467662e40b045e8ee4947
Aleksei Luchinsky <aluchi@bgsu.edu>
2025-07-13 18:05:00 -0400

    login: compare with stored, read display name, check admin

---
+++ js/login.js
@@ -0,0 +1,59 @@
+/************
+ * global window variable where current user name is stored
+ */
+window.currentUser = null;
+
+
+
+
+/**********
+ * Loads stored username at the beginning
+ */
+window.addEventListener("DOMContentLoaded", () => {
+  const stored = localStorage.getItem("currentUser");
+  if (stored) {
+    currentUser = stored;
+    document.getElementById("login-status").textContent = `Logged in as ${currentUser}`;
+    document.getElementById("username").value = stored;
+  }
+});
+
+/******
+ * Logs in with the requested user name
+ */
+async function loginUser() {
+    const input = document.getElementById("username");
+    let name = input.value.trim();
+
+    window.admin = false; // reset admin status
+
+    if (!name) {
+        alert("Please enter your name.");
+        return;
+    }
+
+    // read users data from supabase
+    const {data: usersData, error: usersError} = await client
+    .from("users")
+    .select("*")
+    .eq("login_name", name);
+
+    if( usersError) {
+        console.error("Failed to load user data:", usersError);
+        alert("Failed to load user data. Please try again.");
+        return;
+    }
+
+    if(usersData.length > 0) {
+        name = usersData[0].display_name; // use display name if available
+        window.admin = usersData[0].admin; // set admin status
+    }
+  
+    console.log("usersData", usersData, "usersError", usersError);
+
+
+    window.currentUser = name;
+    localStorage.setItem("currentUser", name);
+    document.getElementById("login-status").textContent = `Logged in as ${window.currentUser}`;
+}
+
