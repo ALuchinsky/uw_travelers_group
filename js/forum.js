4b1d14317ab90db8f39d8b734aa1dad8f7adeec3
Aleksei Luchinsky <aluchi@bgsu.edu>
2025-07-20 08:56:16 -0400

    createNewRoom function moved to forum_functions file

--- js/forum.js
+++ js/forum.js
@@ -342,36 +342,6 @@ async function deleteRoom(room_id, theme_id, theme_topic) {
 }
 /**************** End of deleteRoom */
 
-/****  
- * Creates new room for the particular theme
- */
-async function createNewRoom(theme_id, theme_topic){
-        console.log("Create new room button clicked")
-        const room_topic = prompt("Enter room topic")
-        if(room_topic) {
-            console.log("Creating new room with topic: ", room_topic)
-            const to_insert = {
-                theme_id: theme_id,
-                topic: room_topic,
-                num_messages: 0
-            };
-            client
-                .from('rooms')
-                .insert([to_insert])
-                .then(({ data, error }) => {
-                    if (error) {
-                        console.error("Error creating room:", error);
-                    } else {
-                        console.log("Room created successfully:", data);
-                        renderRooms(theme_id, theme_topic); // Refresh the rooms list
-                    }
-                });
-
-        } else {
-            console.log("Room topic is empty, not creating room")
-        }
-    }
-/***   End of createNewRoom */
 
 /************************
  * Renders messages from particular room off particular topic
--- js/forum_functions.js
+++ js/forum_functions.js
@@ -31,3 +31,34 @@ console.log("Loading forum functions...");
     }
 
 }
+
+/****  
+ * Creates new room for the particular theme
+ */
+async function createNewRoom(theme_id, theme_topic){
+        console.log("Create new room button clicked")
+        const room_topic = prompt("Enter room topic")
+        if(room_topic) {
+            console.log("Creating new room with topic: ", room_topic)
+            const to_insert = {
+                theme_id: theme_id,
+                topic: room_topic,
+                num_messages: 0
+            };
+            client
+                .from('rooms')
+                .insert([to_insert])
+                .then(({ data, error }) => {
+                    if (error) {
+                        console.error("Error creating room:", error);
+                    } else {
+                        console.log("Room created successfully:", data);
+                        renderRooms(theme_id, theme_topic); // Refresh the rooms list
+                    }
+                });
+
+        } else {
+            console.log("Room topic is empty, not creating room")
+        }
+    }
+/***   End of createNewRoom */
