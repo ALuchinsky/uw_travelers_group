ac4a09c252c00b48e66550914a7b3ab36f28f58c
Aleksei Luchinsky <aluchi@bgsu.edu>
2025-07-30 12:11:58 -0400

    ip saved in supabase

--- js/visitors_counter.js
+++ js/visitors_counter.js
@@ -6,6 +6,25 @@ async function getPublicIP() {
   return data.ip;
 }
 
-getPublicIP().then(ip => {
-  console.log("Your IP is:", ip);
-});
+async function logVisit() {
+    ip = "unknown";
+    getPublicIP().then(ip => {
+        console.log("Your IP is:", ip);
+        return ip;
+    })
+    .then(async (ip) => {
+    await client
+        .from("visits")
+        .insert([{ ip_address: ip, timestamp: new Date().toISOString() }])
+        .then(({ data, error }) => {
+            if (error) {
+                console.error("Error logging visit:", error);
+            } else {
+                console.log("Visit logged successfully:", data);
+            }
+        })        
+    });
+
+}
+
+logVisit();
