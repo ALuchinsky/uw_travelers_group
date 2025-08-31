381b4f13bbb0509c8f7fadddbe91d7e73097649d
Aleksei Luchinsky <aluchi@bgsu.edu>
2025-08-31 10:25:33 -0400

    login dialog works

--- js/login.js
+++ js/login.js
@@ -108,3 +108,91 @@ async function loginUser() {
     window.location.reload(); // reload to apply changes
 }
 
+/*********
+* Login Dialog version
+*/
+
+document.body.insertAdjacentHTML("beforeend", `
+<dialog id = "login-dialog">
+    <form method="dialog">
+        <p><label>Enter your name: <input type="text" id="login-name-input"></label></p>
+        <p>Enter password: <input type="password" id="login-password-input"></p>
+        <menu>
+            <button id="login-submit-btn" value="submit">Submit</button>
+            <button id="login-cancel-btn" value="cancel">Cancel</button>
+        </menu>
+    </form>
+</dialog>
+`);
+
+async function login2() {
+  const dialog = document.getElementById("login-dialog");
+  const nameInput = dialog.querySelector("#login-name-input");
+  const passwordInput = dialog.querySelector("#login-password-input");
+  const submitBtn = dialog.querySelector("#login-submit-btn");
+  const cancelBtn = dialog.querySelector("#login-cancel-btn");
+  dialog.addEventListener("close", async () => {
+    if (dialog.returnValue === "submit") {
+      const name = nameInput.value.trim();
+      console.log("Name input:|", name,"|");
+      console.log("Password input:", passwordInput.value);
+      const {data: usersData, error: usersError} = await client
+        .from("users")
+        .select("*")
+        .eq("login_name", name);
+
+      // debug_print("usersData", usersData, "usersError", usersError);
+
+      if( usersError) {
+          console.error("Failed to load user data:", usersError);
+          alert("Failed to load user data. Please try again.");
+          return;
+      }
+
+      if(usersData.length == 0) {
+          alert("User not found.");
+          logVisit(name, "login_error_not_found");
+          doLogin("Guest", false);
+          return;
+      } else {
+          const user = usersData[0];
+          if (user.password) { 
+              if (user.password !== passwordInput.value) {
+                  alert("Incorrect password.");
+                  logVisit(name, "login_admin_failed");
+                  doLogin("Guest", false);
+                  return;
+              } else {
+                  logVisit(name, "login_admin_success");
+                  doLogin(user.display_name || name, user.admin);
+                  return;
+              }
+          } else {
+              logVisit(name, "login_user_success");
+              doLogin(user.display_name || name, false);
+              return;
+            }
+      }
+
+    }
+  });
+  dialog.showModal();
+  console.log("Dialog shown");
+}
+
+function doLogin(name, isAdmin) {
+  window.currentUser = name;
+  window.admin = isAdmin;
+  localStorage.setItem("currentUser", name);
+  localStorage.setItem("admin", window.admin);
+  document.getElementById("login-status").textContent = `Logged in as ${window.currentUser}`;
+  let message = `Welcome to the Travel Club, ${name.split(" ")[0]}!`;
+  if( name === "Guest") {
+      message = "Welcome to the Travel Club! Your name and/or password was not recognized. Please log in again or contact the site administrator.";
+      alert(message);
+      renderThemes(); // refresh themes to show guest view
+      return;
+  }
+  alert(message); // welcome message
+  window.location.reload(); // reload to apply changes
+}
